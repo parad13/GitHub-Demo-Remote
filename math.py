95c726f765758d31f4eb0e0f1fb9ee32cb885180
= <=>
2022-01-22 22:17:11 +0530

    Implemented multiplication() fn locally on branch bug456

--- math.py
+++ math.py
@@ -1,13 +1,13 @@
 # Addition fn implemention
 def addition(a):
   return x+y
-
+ 
 # Subtraction fn implementation
 def subtraction(a):
   return x-y # on bug456 branch
 
 def multiplication(a):
-  pass
+  return x*y
 
 def divide(a):
   pass
